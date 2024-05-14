05d5e7de42580472ea25ad4fbacdb8bc18333aab
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-05-14 23:24:17 +0900

    Fix dc01.py

--- src/pwn10eb12cb/dc01.py
+++ src/pwn10eb12cb/dc01.py
@@ -9,41 +9,32 @@ class DcMotor:
         GPIO.setup(self.in1, GPIO.OUT)
         GPIO.setup(self.in2, GPIO.OUT)
 
-    def mode_view(self):
-        modes_operation = {
-            (0, 0): 'ストップ(TSD, ISD解除)',
-            (0, 1): '正転(正転)',
-            (1, 0): '逆転(逆転)',
-            (1, 1): 'ブレーキ(ブレーキ)'
-        }
-        print(f'モード：{modes_operation[mode]}, 入力(IN1, IN2)：({self.in1}, {self.in2}->{"H" if self.in1 else "L"}, {"H" if self.in2 else "L"})')
-
     def run(self):
         GPIO.output(20, GPIO.LOW)
         GPIO.output(21, GPIO.LOW)
-        print('初期条件')
+        print("初期条件")
         print_mode((GPIO.input(20), GPIO.input(21)))
-        time.sleep(1) #1秒待つ
-        print(f'{"#"*20}\n')
+        time.sleep(1)
+        print("######################")
 
         try:
-            while True:
-                # 正転モード
+            for i in range(5):
+                print("正転モード")
                 GPIO.output(20, GPIO.HIGH)
                 GPIO.output(21, GPIO.LOW)
                 print_mode((GPIO.input(20), GPIO.input(21)))
-                time.sleep(3
-                # ストップモード
+                time.sleep(3)
+                print("ストップモード")
                 GPIO.output(20, GPIO.LOW)
                 GPIO.output(21, GPIO.LOW)
                 print_mode((GPIO.input(20), GPIO.input(21)))
-                time.sleep(1
-                # 逆転モード
+                time.sleep(1)
+                print("逆転モード")
                 GPIO.output(20, GPIO.LOW)
                 GPIO.output(21, GPIO.HIGH)
                 print_mode((GPIO.input(20), GPIO.input(21)))
-                time.sleep(3
-                # ブレーキモード
+                time.sleep(3)
+                print("ブレーキモード")
                 GPIO.output(20, GPIO.HIGH)
                 GPIO.output(21, GPIO.HIGH)
                 print_mode((GPIO.input(20), GPIO.input(21)))
