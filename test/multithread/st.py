c213e1e6f4bb057112c95bbc7e81a43bc5a241ad
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-06-12 11:30:00 +0900

    fix multithread

--- test/multithread/st.py
+++ test/multithread/st.py
@@ -1,9 +1,31 @@
 import threading
-import Led
+import time
+import RPi.GPIO as GPIO
+
+class Led:
+    def __init__(self, pin=18, wait=3):
+        self.pin = pin
+        self.wait = wait
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
+
+    def run():
+        print(f'pin:{self.pin} start')
+        for i in range(5):
+            GPIO.output(pin, True)
+            time.sleep(wait)
+            GPIO.output(pin, False)
+            time.sleep(2)
+
+        GPIO.cleanup(pin)
+        print(f'pin:{self.pin} finish')
+
 
 
 def run(pin, wait):
-    a = Led.Led(pin, wait)
+    a = Led(pin, wait)
     a.run()
 
 if __name__ == '__main__':
